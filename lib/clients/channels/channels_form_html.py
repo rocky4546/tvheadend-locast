a143f06a89447b1cca75d14c18d416e75b72c742
rocky4546 <24759693+rocky4546@users.noreply.github.com>
2021-05-21 13:34:10 -0500

    Reduced memory on xmltv.xml requests

--- lib/clients/channels/channels_form_html.py
+++ lib/clients/channels/channels_form_html.py
@@ -7,7 +7,7 @@ https://github.com/rocky4546
 This file is part of Cabernet
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-and associated documentation files (the “Software”), to deal in the Software without restriction,
+and associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute,
 sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
 is furnished to do so, subject to the following conditions:
@@ -234,7 +234,7 @@ class ChannelsFormHTML:
             '<center>metadata</center>'
             '<ul>',
             '<li style="list-style-type:none;">',
-            '<input id="text-mi" name="metadata-checkbox" type=checkbox> Filter: <input name="metadata-text" type=text size=7>',
+            self.get_filter_text_chkbox('metadata'), ' Filter: ', self.get_filter_textbox('metadata'),
             '</li>',
             '</ul>',
             '</div>',
