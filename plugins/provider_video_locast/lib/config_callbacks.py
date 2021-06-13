397debfdf711d1813509725822a9590cc692eed6
mccaslin <24759693+rocky4546@users.noreply.github.com>
2021-06-13 10:08:26 -0500

    CoreELEC and Windows updates

--- plugins/provider_video_locast/lib/config_callbacks.py
+++ plugins/provider_video_locast/lib/config_callbacks.py
@@ -3,5 +3,8 @@ from .channels import Channels
 from lib.plugins.plugin_handler import PluginHandler
 
 def force_channelsdb_refresh(_config_obj, _section, _key):
-    locast_obj = PluginHandler.plugins['Locast'].plugin_obj
-    locast_obj.channels.refresh_channels(True)
+    locast_obj = PluginHandler.cls_plugins['Locast'].plugin_obj
+    instance = _section.split('_', 1)[1]
+    locast_obj.locast_instances[instance].channels.refresh_channels(True)
+
+
